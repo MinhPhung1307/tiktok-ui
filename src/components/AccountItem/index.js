6684e6dd9e68715ec6d074d2a96bd06a21f718e9
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2025-04-19 21:57:01 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,12 +2,13 @@ import classNames from 'classnames/bind';
 import styles from './AcountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 function AcountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/7068087176885436442~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=6950e73c&x-expires=1745046000&x-signature=xHLALwywf3wqGZwyFNDpaCTYADk%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
                 alt=""
